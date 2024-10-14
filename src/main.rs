a1372ddb020ac90250aa5ae65dbfefeb0264f73a
lou <dustyfingers01257@gmail.com>
2024-10-13 23:30:44 -0600

    learn about structs and impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::Hashmap;
+
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("Please specify an action:");
+    let item = std::env::args().nth(2).expect("Please specify an item:");
+
+    println!("{:?} {:?}", action, item);
+}
+
+// structs are like types in typescript
+struct Todo {
+    // use rust built in hashmap to store kv pairs
+    map: HashMap<String, bool>,
+}
+
+// impl holds the types 'methods'
+impl Todo {
+    // insert a new item into the map
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
 }
